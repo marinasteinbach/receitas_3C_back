4ded8de97cf1877f5338bc835d9e384397f4ad5f
MARINA ESPÍNDOLA STEINBACH <marina_steinbach@estudante.sesisenai.org.br>
2026-03-02 12:01:26 -0300

    PA

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 import Fastify from 'fastify'
-const servidor = Fastify()
-servidor.get('/usuarios',()=>{
-    return 'Está funcionando!'
+import { Pool } from 'pg'
+
+
+const sql = new Pool({
+    user: "postgres",
+    password: "senai",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
 })
+
+
+const servidor = Fastify()
+
+
+servidor.get('/usuarios', async () => {
+    const resultado = await sql.query('select * from usuario')
+    return resultado.rows
+})
+servidor.post ('/usuarios', async (request, reply) => {
+    const nome = request.body.nome;
+    const senha = request.body.senha;
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha)VALUES ($1, $2)', [nome, senha])
+    return 'usuario cadastrado'
+
+})
+
+
 servidor.listen({
     port: 3000
 })
